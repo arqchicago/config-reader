3e2569ed5a1dec9393f9d209dd4c5d836d34f071
Ahmad Qadri <arqchicago@gmail.com>
2021-08-05 21:34:39 -0400

    reading integer parameters

--- test_config.py
+++ test_config.py
@@ -3,15 +3,22 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+# config.getint: to read integer parameters
+print(f'integers')
+print(f'> parameter 1 = {config.getint("integer_vars", "parameter1")}')
+print(f'> parameter 2 = {config.getint("integer_vars", "parameter2")}')
+print(f'> parameter 3 = {config.getint("integer_vars", "parameter3")}')
+print(f'> parameter 4 = {config.getint("integer_vars", "parameter4")}\n')
+
 # config.getfloat: to read floating point parameters
-print(f'> parameter 1 = {config.getfloat("floats", "parameter1")}')
-print(f'> parameter 2 = {config.getfloat("floats", "parameter2")}')
-print(f'> parameter 3 = {config.getfloat("floats", "parameter3")}')
-print(f'> parameter 4 = {config.getfloat("floats", "parameter4")}')
+print(f'floats')
+print(f'> parameter 1 = {config.getfloat("float_vars", "parameter1")}')
+print(f'> parameter 2 = {config.getfloat("float_vars", "parameter2")}')
+print(f'> parameter 3 = {config.getfloat("float_vars", "parameter3")}')
+print(f'> parameter 4 = {config.getfloat("float_vars", "parameter4")}')
 
 # config.get: to read string parameters
 # config.getboolean: to read boolean parameters
-# config.getint: to read integer parameters
 
 #with open('config.ini', 'w') as config_file:
 #    config.write(config_file)
