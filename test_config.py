ce6264baef2ba920556b25019dfa1ae93aac435e
Ahmad Qadri <arqchicago@gmail.com>
2021-08-10 21:39:44 -0400

    reading string parameters

--- test_config.py
+++ test_config.py
@@ -15,9 +15,15 @@ print(f'floats')
 print(f'> parameter 1 = {config.getfloat("float_vars", "parameter1")}')
 print(f'> parameter 2 = {config.getfloat("float_vars", "parameter2")}')
 print(f'> parameter 3 = {config.getfloat("float_vars", "parameter3")}')
-print(f'> parameter 4 = {config.getfloat("float_vars", "parameter4")}')
+print(f'> parameter 4 = {config.getfloat("float_vars", "parameter4")}\n')
 
 # config.get: to read string parameters
+print(f'strings')
+print(f'> parameter 1 = {config.get("string_vars", "parameter1")}')
+print(f'> parameter 2 = {config.get("string_vars", "parameter2")}')
+print(f'> parameter 3 = {config.get("string_vars", "parameter3")}')
+print(f'> parameter 4 = {config.get("string_vars", "parameter4")}')
+
 # config.getboolean: to read boolean parameters
 
 #with open('config.ini', 'w') as config_file:
